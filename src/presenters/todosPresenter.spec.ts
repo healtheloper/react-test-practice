09898572ea6b52b5fb303753cc5f3560ad158c45
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-07-22 11:19:58 +0900

    test: add, delete todo

--- src/presenters/todosPresenter.spec.ts
+++ src/presenters/todosPresenter.spec.ts
@@ -8,12 +8,28 @@ describe('TodosPresenter', () => {
     },
   ];
   let todosPresenter;
-
+  let update;
   beforeEach(() => {
     todosPresenter = new TodosPresenter(todos);
+    update = jest.fn();
   });
 
   it('init with todos', () => {
     expect(todosPresenter.getTodos()).toEqual(todos);
+  });
+
+  it('add todo', () => {
+    const newTodo = {
+      id: 2,
+      title: '두번째 투두',
+    };
+    todosPresenter.add(newTodo.title, update);
+    const newTodos = [...todos, newTodo];
+    expect(todosPresenter.getTodos()).toEqual(newTodos);
+  });
+
+  it('delete todo', () => {
+    todosPresenter.delete(1, update);
+    expect(todosPresenter.getTodos()).toEqual([]);
   });
 });
